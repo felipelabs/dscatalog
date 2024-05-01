10188df40cbe1164f2aec5bfcbcada27261c40aa
Felipe Neves <felipelabs@gmail.com>
2024-04-30 21:21:56 -0300

    Add seed data

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResources.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResources.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class CategoryResources {
     @Autowired
     CategoryService categoryService;
 
+    @Transactional(readOnly = true)
     public ResponseEntity<List<Category>> findAll(){
         return ResponseEntity.ok().body(categoryService.findAll());
     }
