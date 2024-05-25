adeda7e1a533ad634e67045101a48a48af451156
Felipe Neves <felipelabs@gmail.com>
2024-05-24 22:08:42 -0300

    Integration test put

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscatalog.resources;
 
+import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +24,20 @@ public class ProductResourceIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private long existingId;
     private long nonExistingId;
     private long countTotalProducts;
+    private String jsonBody;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         nonExistingId = 100L;
         countTotalProducts = 25;
+        jsonBody = objectMapper.writeValueAsString(Factory.createProductDTO());
     }
 
     @Test
@@ -39,4 +46,26 @@ public class ProductResourceIT {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.totalElements").value(countTotalProducts));
     }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception{
+        String expectedName = Factory.createProductDTO().getName();
+        String expectedDescription = Factory.createProductDTO().getDescription();
+
+        mockMvc.perform(put("/products/{id}", existingId).content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.name").value(expectedName))
+                .andExpect(jsonPath("$.description").value(expectedDescription));
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExists() throws Exception{
+        String expectedName = Factory.createProductDTO().getName();
+        String expectedDescription = Factory.createProductDTO().getDescription();
+
+        mockMvc.perform(put("/products/{id}", nonExistingId).content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
 }
